ff26be6e5a46888e66db63012fa11f8d91ae9b58
Yurupari <cristhianmarin15@gmail.com>
2026-02-12 22:02:09 +0100

    Initial commit

---
+++ src/main/java/com/haiilo/kata/backend/KataApplication.java
@@ -0,0 +1,13 @@
+package com.haiilo.kata.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataApplication.class, args);
+	}
+
+}
